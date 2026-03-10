170d3822c08953ad0627840e0b814888b6b9da9d
Daniel Duishenbekov <dujsenbekovdaniel8@gmail.com>
2026-03-11 02:33:17 +0600

    f

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,10 +4,10 @@ import Principles from "./principles/Principles";
 import OurDoctors from "./ourDoctors/OurDoctors";
 import Interior from "./Interior/Interior";
 import Cta from "./cta/Cta";
-import ClinicsMap from "./adress/ClinicsMap";
 import Review from "./review/Review";
 import { ReviewProvider } from "../../context/ReviewContext";
 import Documents from "./document/Documents";
+import ClinicsMap from "./address/ClinicsMap";
 // import scss from "./Aboute.module.scss"
 const About = () => {
   return (
